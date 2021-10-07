1dc848baf9f87ed6b5c11f2d295834b3bc45e6c1
Asku_SU <zaharandrei15@mail.ru>
2021-10-07 09:49:10 +0300

    Добавил стартовые файлы

---
+++ HW_L4.cpp
@@ -0,0 +1,51 @@
+﻿#include <windows.h>
+#include "MyLib.h"
+
+
+
+void Task1()
+{
+	std::cout << "Работа с Функцией push_back_average:" << std::endl << std::endl;
+	
+	std::cout << std::endl;
+}
+
+void Task2()
+{
+	std::cout << "Работа с Классом Matrix:" << std::endl << std::endl;
+	
+}
+
+
+void Task3()
+{	
+	std::cout << "Работа с Классом Range:" << std::endl << std::endl;
+	
+}
+
+
+int main()
+{
+	setlocale(LC_ALL, "RU");
+
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	
+	unsigned short taskNumber = 0;
+	while (true)
+	{
+		taskNumber = myLib::getUserSelectedTask(3);
+		switch (taskNumber)
+		{
+		case 1: Task1();
+			break;
+		case 2: Task2();
+			break;
+		case 3: Task3();
+			break;
+		}
+		std::cout << std::endl;
+	}
+
+    return 0;
+}
