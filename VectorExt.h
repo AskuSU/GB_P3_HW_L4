da154a3796be894157cc651959f5510930dc7ddb
Asku_SU <zaharandrei15@mail.ru>
2021-10-08 13:11:28 +0300

    Изменил вставку в контейнер

--- VectorExt.h
+++ VectorExt.h
@@ -5,5 +5,5 @@ template <typename Container, typename Value>
 void insert_sorted(Container& cont, const Value& val)
 {
 	auto it = std::upper_bound(cont.begin(), cont.end(), val);
-	std::fill_n(std::inserter(cont, it), 1, val);
+	cont.insert(it, val);
 }
